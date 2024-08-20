33cf1a53e52929f8aaa616ed60fdc14752757f99
Jo <johj703@nate.com>
2024-08-20 22:37:53 +0900

    순서에 따라 configStore 만들기

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,8 +1,10 @@
-import { combineReducers } from "redux";
+import { combineReducers, createStore } from "redux";
 
 // 1) rootREducer를 만든다
 const rootReducer = combineReducers({});
 
 // 2) store를 조합한다.
+const store = createStore(rootReducer);
 
 // 3) 만든 store를 내보낸다.
+export default store;
