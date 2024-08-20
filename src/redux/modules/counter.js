a1c8bf76b4fb5b7462ce8ea62a2679f5247d7d44
Jo <johj703@nate.com>
2024-08-20 22:44:22 +0900

    counter 리듀서 생성

---
+++ src/redux/modules/counter.js
@@ -0,0 +1,13 @@
+const initialState = {
+  number: 0,
+};
+
+// 리듀서(함수)
+const counter = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      state;
+  }
+};
+
+export default counter;
