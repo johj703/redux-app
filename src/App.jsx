9aee2419074d2256a07cb30ef8dc3b49187e7799
Jo <johj703@nate.com>
2024-08-20 23:03:25 +0900

    useSelector를 import하고 callback함수로 state를 콘솔로 확인

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const counterReducer = useSelector((state) => {
+    state;
+  });
+
+  console.log("state", counterReducer);
+
   return <div>App</div>;
 };
 
