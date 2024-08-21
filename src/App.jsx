715cb3625b4ac2b7b8268348386bb43c470fecd2
Jo <johj703@nate.com>
2024-08-21 16:59:40 +0900

    counterReducer를 한 줄로 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const App = () => {
-  const counterReducer = useSelector((state) => {
-    return state;
-  });
-
+  const counterReducer = useSelector((state) => state.counter);
   console.log("state", counterReducer);
 
   return <div>App</div>;
