10afa3ed03bd56997bc84a70f7e8b43616d33aa1
Jo <johj703@nate.com>
2024-08-21 19:47:52 +0900

    주석으로 막아놨던 counterReducer를 풀어서 +1버튼을 클릭하면 state가 변경되는 것을 확인

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-// import React from "react";
-// import { useSelector } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 
 import { useDispatch } from "react-redux";
 
 const App = () => {
-  // const counterReducer = useSelector((state) => state.counter);
-  // console.log("state", counterReducer);
+  const counterReducer = useSelector((state) => state.counter);
+  console.log("state", counterReducer);
   const dispatch = useDispatch();
 
   return (
