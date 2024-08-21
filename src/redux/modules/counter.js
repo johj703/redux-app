e1803d9d6458def794f6fee15f2916c245bd3397
Jo <johj703@nate.com>
2024-08-21 20:11:50 +0900

    MINUS_ONE도 추가를 해서 리듀서에 추가 및 case에 적었던 string을 함수명으로 변경

--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -4,6 +4,7 @@ const initialState = {
 };
 
 const PLUS_ONE = "PLUS_ONE";
+const MINUS_ONE = "MINUS_ONE";
 
 export const plusOne = () => {
   return {
@@ -11,15 +12,21 @@ export const plusOne = () => {
   };
 };
 
+export const minusOne = () => {
+  return {
+    type: MINUS_ONE,
+  };
+};
+
 // 리듀서(함수)
 const counter = (state = initialState, action) => {
   console.log("action => ", action);
   switch (action.type) {
-    case "PLUS_ONE":
+    case PLUS_ONE:
       return {
         number: state.number + 1,
       };
-    case "MINUS_ONE":
+    case MINUS_ONE:
       return {
         number: state.number - 1,
       };
