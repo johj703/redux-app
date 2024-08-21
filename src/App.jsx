d25dafaa67fb45eeb1c2e26140b1241992e6bd04
Jo <johj703@nate.com>
2024-08-21 19:35:27 +0900

    useDispatch함수를 dispatch 상수에 할당

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from "react";
-import { useSelector } from "react-redux";
+// import React from "react";
+// import { useSelector } from "react-redux";
+
+import { useDispatch } from "react-redux";
 
 const App = () => {
-  const counterReducer = useSelector((state) => state.counter);
-  console.log("state", counterReducer);
+  // const counterReducer = useSelector((state) => state.counter);
+  // console.log("state", counterReducer);
+  const dispatch = useDispatch();
 
-  return <div>App</div>;
+  return (
+    <div>
+      <button>+1</button>
+    </div>
+  );
 };
 
 export default App;
