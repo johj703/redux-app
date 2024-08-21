48834c21e16f66a2a7f97b1302958f2273102316
Jo <johj703@nate.com>
2024-08-21 20:28:03 +0900

    count state를 만들고 input태그에 value와 onChange 함수 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import { React, useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { addNumber, removeNumber } from "./redux/modules/counter.js";
 
 const App = () => {
+  const [count, setCount] = useState(0);
   const counterReducer = useSelector((state) => state.counter);
   console.log("state", counterReducer);
   const dispatch = useDispatch();
@@ -11,7 +12,13 @@ const App = () => {
   return (
     <div>
       {counterReducer.number}
-      <input type="number" />
+      <input
+        type="number"
+        value={count}
+        onChange={(e) => {
+          setCount(e.target.value);
+        }}
+      />
       <button
         onClick={() => {
           dispatch(addNumber());
