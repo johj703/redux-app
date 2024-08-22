e94dc01f83f3cc74de5eed9c9637ac0c753e7888
Jo <johj703@nate.com>
2024-08-22 15:10:53 +0900

    기존에 있던 코드들을 주석처리하고 새로 counterSlice를 포함하는 reducer를 store에 담는 변수 생성

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,13 +1,22 @@
-import { combineReducers, createStore } from "redux";
-import counter from "../modules/counter";
+// import { combineReducers, createStore } from "redux";
+// import counter from "../modules/counter";
 
-// 1) rootReducer를 만든다
-const rootReducer = combineReducers({
-  counter,
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "../slices/counterSlice";
+
+// // 1) rootReducer를 만든다
+// const rootReducer = combineReducers({
+//   counter,
+// });
+
+// // 2) store를 조합한다.
+// const store = createStore(rootReducer);
+
+const store = configureStore({
+  reducer: {
+    counter: counterSlice,
+  },
 });
-
-// 2) store를 조합한다.
-const store = createStore(rootReducer);
 
 // 3) 만든 store를 내보낸다.
 export default store;
